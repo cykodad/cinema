5f243ee4cb5451ba90dcc52b5a64a90ee0c48e3a
Mehmet Bulut <mail@mehmetbulut.net>
2020-05-08 01:41:55 +0200

    video 23 completed

--- src/main.js
+++ src/main.js
@@ -1,19 +1,29 @@
 import Vue from 'vue';
-import "./style.scss"
+import "./style.scss";
+import MovieList from "./components/MovieList.vue";
+import MovieFilter from "./components/MovieFilter.vue";
 
 new Vue({
   el: "#app",
-  components: {
-    "movie-list": {
-      template: `<div id="movie-list">
-                    Movie list
-                 </div>`
-    },
-    "movie-filter": {
-      template: `<div id="movie-filter">
-                    Movie filter
-                 </div>`
+  data: {
+    genre: [],
+    time: []
+  },
+  methods: {
+    checkFilter(category, title, checked) {
+      if (checked) {
+        this[category].push(title);
+      } else {
+        let index = this[category].indexOf(title);
+        if (index >= 0) {
+          this[category].splice(index, 1);
+        }
+      }
     }
+  },
+  components: {
+    MovieList,
+    MovieFilter
   }
 }
 );
